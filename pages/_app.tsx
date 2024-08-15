765597f517290457f432410992108e6c20d0c0f2
[SW7] 홍소현 <hshyeon86@gmail.com>
2024-08-15 22:11:25 +0900

    initial setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../src/store";
-import  "./global.css";
+import HeaderComp from "../src/components/HeaderComp";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../global/theme";
+import GlobalStyle from "../global/global";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <main>
+          <HeaderComp />
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </Provider>
   );
 }
