385e4e6134eac2a48ac39f84fc3100440429413e
[SW7] 홍소현 <hshyeon86@gmail.com>
2024-08-16 20:34:44 +0900

    chore: fix to styled component

--- src/components/StoryComp.tsx
+++ src/components/StoryComp.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./index.module.css";
+import styled from "styled-components";
 
 interface IgStory {
   uri: string;
@@ -22,52 +22,65 @@ const StoryComp = ({ story }: StoryComponentProps) => {
   };
 
   useEffect(() => {
-    // If the story is marked as invalid, skip rendering
     if (!isValid) {
       return;
     }
   }, [isValid]);
 
   if (!isValid) {
-    return null; // Do not render anything if the image is invalid
+    return null;
   }
   const fileExtension = story.uri ? story.uri.split(".").pop() : "";
   return (
     <>
-  
-        <div className={styles.storyContainer} key={story.uri}>
-          <p>{new Date(story.creation_timestamp * 1000).toLocaleString()}</p>
-          <div className={styles.content}>
-            {story.uri ? (
-              fileExtension === "mp4" ||
-              fileExtension === "mov" ||
-              fileExtension === "avi" ? (
-                <video src={story.uri} controls className={styles.storyVideo} 
-                onError={handleError}/>
-              ) : (
-                <img
-                  src={story.uri}
-                  alt="Story Image"
-                  className={styles.storyImage}
-                  onError={handleError}
-                />
-              )
+      <div key={story.uri}>
+        <p>{new Date(story.creation_timestamp * 1000).toLocaleString()}</p>
+        <Content>
+          {story.uri ? (
+            fileExtension === "mp4" ||
+            fileExtension === "mov" ||
+            fileExtension === "avi" ? (
+              <StoryVideo src={story.uri} controls onError={handleError} />
             ) : (
-              <p>
-                {story.text.split("\n").map((line, index) => (
-                  <React.Fragment key={index}>
-                    {index > 0 && <br />}
-                    {line}
-                    <br />
-                  </React.Fragment>
-                ))}
-              </p>
-            )}
-          </div>
-        </div>
-      
+              <StoryImg
+                src={story.uri}
+                alt="Story Image"
+                onError={handleError}
+              />
+            )
+          ) : (
+            <p>
+              {story.text.split("\n").map((line, index) => (
+                <React.Fragment key={index}>
+                  {index > 0 && <br />}
+                  {line}
+                  <br />
+                </React.Fragment>
+              ))}
+            </p>
+          )}
+        </Content>
+      </div>
     </>
   );
 };
 
 export default StoryComp;
+
+const Content = styled.div`
+  height: 500px;
+  background-color: black;
+  color: white;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  line-height: 1.2rem;
+  overflow: auto;
+`;
+const StoryImg = styled.img`
+  height: 500px;
+`;
+const StoryVideo = styled.video`
+  height: 500px;
+`;
